d6b7b611dfc0bb0175c339a8c5e72d2eb4751438
loopasam <samuel.croset@gmail.com>
2023-10-09 12:37:25 +0200

    projectiles

--- game.py
+++ game.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-
 @app.route("/mario")
 def mario():
     return render_template('mario.html')
+
+@app.route("/projectile")
+def projectile():
+    return render_template('projectiles.html')
 
 @app.route("/tile_explorer")
 def tile_explorer():
